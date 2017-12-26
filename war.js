d1a16146a42ce235372109322faf04387f8c660c
Christopher Chin <Chris.ChinRx@gmail.com>
2017-12-26 11:34:58 -0500

    New Version being worked on

--- war.js
+++ war.js
@@ -1,42 +1,27 @@
-// Creates the card deck and creates a split between player and cpu 
-var warArray [], playerHand [], cpuHand [];
-var playDeck = '', cpuDeck = '', playerCard = '', cpuCard = '';
-var playing = false;
+var suit = ['&spades;', '&hearts;', '&diams;', '&clubs;'];
+var cardType = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+var deck[];
 
-//populating deck 
-function fillArray() {
-        var deck = [];
-        for (var i = 0; i < 52; i++)
-            deck[i] = i;
-    
-        shuffle(deck);
-        splitCards(deck);
+var player = document.getElementById('player');
+var cpu = document.getElementById('cpu');
+
+var start = document.getElementById('start');
+var play = document.getElementById('play');
+var shuffle = document.querySelector('P');
+
+var buildDeck = function(suit, cardType) {
+    for (var i = 0; i < suit.length; i++) {
+        for (var j = 0; j < cardType.length; j++) {
+            deck.push({
+                suit: suits[i],
+                cardType: cardType[j],
+                value: j
+            })
+        }
     }
-//Deck shuffling 
-function shuffle(deck) {
-    for(var j, x, i = deck.length; i; j = Math.floor(Math.random() * i), x = deck[--i], deck[i] = deck[j], deck[j] = x);
-    return deck;
-}
+    shuffle(deck);
+};
 
-//function to split shuffled deck in half
-function splitCards(deck) {
-	var i = 0;
+function shuffle(array) {
 
-	//push a card to each "hand" array
-	while (i != deck.length) {
-		playerHand.push(deck[i]);
-		cpuHand.push(deck[(i+1)]);
-		i+=2;
-	}
-
-	$('.playCount').html("Player cards: " + playerHand.length);
-	$('.compCount').html("Computer cards: " + cpuHand.length);
-	$('.result').html("");
-}
-function deal() {
-    $('.playerCard').html("");
-	$('.compCard').html("");
-	$('.newGame').show();
-
-    
 }
