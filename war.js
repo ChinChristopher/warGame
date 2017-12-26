f0612adf453281bde5b383bac13637a633e611bc
Christopher Chin <Chris.ChinRx@gmail.com>
2017-12-26 16:37:09 -0500

    Add files via upload

--- war.js
+++ war.js
@@ -1,6 +1,6 @@
 var suit = ['&spades;', '&hearts;', '&diams;', '&clubs;'];
 var cardType = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
-var deck[];
+var deck[cardType, suit];
 
 var player = document.getElementById('player');
 var cpu = document.getElementById('cpu');
@@ -23,5 +23,19 @@ var buildDeck = function(suit, cardType) {
 };
 
 function shuffle(array) {
+    var x = array.length,
+        t, i;
 
+    while (x) {
+        i = Math.floor(math.random() * x--);
+
+        t = array[x];
+        array[x] = array[i];
+        array[i] = t;
+    }
+    return array;
 }
+}
+
+var playerHand = [];
+var cpuHand = [];
